aiven/aiven-client

commit 5bb4c61e9950de4c8c000a4ab02b0c901e0b06ff

Migrate from deprecated imp to importlib

version.py
@@ -3,7 +3,7 @@ automatically maintains the latest git tag + revision info in a python file
 
 """
 
-import imp
+import importlib
 import os
 import subprocess
 
@@ -11,7 +11,7 @@ import subprocess
 def get_project_version(version_file):
     version_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), version_file)
     try:
-        module = imp.load_source("verfile", version_file)
+        module = importlib.load_module(version_file)
         file_ver = module.__version__
     except:  # pylint: disable=bare-except
         file_ver = None

commit 0143476582236f2afb92baabac5a6c45a4c2c002

Fix version reading
importlib doesn't have load_module and fetching version from version.py
always failed, causing pip installation to fail. Regression from
5bb4c61e9950de4c8c000a4ab02b0c901e0b06ff

version.py
@@ -3,7 +3,7 @@ automatically maintains the latest git tag + revision info in a python file
 
 """
 
-import importlib
+import importlib.machinery
 import os
 import subprocess
 
@@ -11,7 +11,7 @@ import subprocess
 def get_project_version(version_file):
     version_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), version_file)
     try:
-        module = importlib.load_module(version_file)
+        module = importlib.machinery.SourceFileLoader("version", version_file).load_module()
         file_ver = module.__version__
     except:  # pylint: disable=bare-except
         file_ver = None