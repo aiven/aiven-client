02be844827cc69a4960256eb742ffea8470d52a2
Alexander Olekhnovich <alex@aiven.io>
2021-08-13 15:23:44 +0200

    Add couple of tests for service task commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,7 @@
 # pylint: disable=no-member
 from aiven.client.cli import AivenCLI
 from collections import namedtuple
+from unittest import mock
 
 import pytest
 
@@ -107,3 +108,69 @@ def test_create_user_config():
         "foo": None,
         "main": 3,
     }
+
+
+@pytest.fixture(name="authenticated_cli")
+def fixture_authenticated_cli():
+    def patched_get_auth_token():
+        return "token"
+
+    cli = AivenCLI()
+    cli._get_auth_token = patched_get_auth_token  # pylint: disable=protected-access
+    return cli
+
+
+def test_service_task_create_migration_check(authenticated_cli):
+    task_create_response = {
+        "message": "created",
+        "task": {
+            "create_time": "2021-08-13T08:00:45Z",
+            "result": "",
+            "success": None,
+            "task_id": "79803598-d09a-44bf-ae2b-34aad942f4e8",
+            "task_type": "mysql_migration_check"
+        }
+    }
+
+    with mock.patch("aiven.client.client.AivenClient") as cli_client_mock:
+        cli_client_mock.return_value.create_service_task.return_value = task_create_response
+        authenticated_cli.run(
+            args=[
+                "service", "task-create", "--operation", "migration_check", "--project", "test", "--source-service-uri",
+                "mysql://root:password@source-mysql-server:3306/", "--ignore-dbs", "db1", "target-mysql-service-1"
+            ]
+        )
+        cli_client_mock.return_value.create_service_task.assert_called_with(
+            project='test',
+            service='target-mysql-service-1',
+            body={
+                'task_type': 'migration_check',
+                'migration_check': {
+                    'source_service_uri': 'mysql://root:password@source-mysql-server:3306/',
+                    'ignore_dbs': 'db1'
+                }
+            }
+        )
+
+
+def test_service_task_get_migration_check(authenticated_cli):
+    task_get_response = {
+        "create_time": "2021-08-13T07:10:35Z",
+        "ignore_dbs": "defaultdb",
+        "result": "aiven_mysql_migrate.exceptions.NothingToMigrateException: No databases to migrate",
+        "success": False,
+        "task_id": "f25eac03-25f7-4de2-be6a-eb476fec1730",
+        "task_type": "mysql_migration_check"
+    }
+
+    with mock.patch("aiven.client.client.AivenClient") as cli_client_mock:
+        cli_client_mock.return_value.get_service_task.return_value = task_get_response
+        authenticated_cli.run(
+            args=[
+                "service", "task-get", "--task-id", "f25eac03-25f7-4de2-be6a-eb476fec1730", "--project", "test",
+                "target-mysql-service-1"
+            ]
+        )
+        cli_client_mock.return_value.get_service_task.assert_called_with(
+            project='test', service='target-mysql-service-1', task_id='f25eac03-25f7-4de2-be6a-eb476fec1730'
+        )
