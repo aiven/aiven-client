9a07b94d0b926961a85b69708d715e5bcbb8d933
Marko Teiste <marko.teiste@aiven.io>
2023-03-28 14:11:56 +0300

    Add support for Opensearch Security Management

--- aiven/client/cli.py
+++ aiven/client/cli.py
@@ -3092,6 +3092,70 @@ ssl.truststore.type=JKS
 
     @arg.project
     @arg.service_name
+    def service__opensearch_security_management__status(self) -> None:
+        """Show status of the opensearch security"""
+        response = self.client.opensearch_security_get(
+            project=self.get_project(),
+            service=self.args.service_name,
+        )
+        available = response.get("security_plugin_available", False)
+        admin_enabled = response.get("security_plugin_admin_enabled", False)
+        message = response.get("message")
+        if not available:
+            if not message:
+                print("Opensearch Security is not available for the service")
+                return
+            print(message)
+            return
+        if admin_enabled:
+            print("Opensearch Security Management enabled")
+            return
+        print("Opensearch Security Management disabled")
+
+    @arg.project
+    @arg.service_name
+    def service__opensearch_security_management__set(self) -> None:
+        """Set the password for the opensearch security management"""
+        print("Opensearch Security Management is enabled by setting the password")
+        print("for the security management user. Once enabled normal Aiven service")
+        print("user and ACL management is no longer used and all user and access control")
+        print("can only be done using the native Opensearch Security API or dashboard.")
+        if self.confirm("this action is unrevertable. Proceed (y/N)? "):
+            passwd = self.enter_password(
+                prompt="Setup Opensearch Security Manager",
+                var="AIVEN_OS_SECOP_PASSWORD",
+                confirm=True,
+            )
+            response = self.client.opensearch_security_set(
+                project=self.get_project(),
+                service=self.args.service_name,
+                password=passwd,
+            )
+            print(response.get("message"))
+
+    @arg.project
+    @arg.service_name
+    def service__opensearch_security_management__reset(self) -> None:
+        """Reset the password for the opensearch security management"""
+        old_passwd = self.enter_password(
+            prompt="Old Opensearch Security Manager password",
+            var="AIVEN_OS_SECOP_PASSWORD",
+        )
+        new_passwd = self.enter_password(
+            prompt="New password",
+            var="AIVEN_OS_SECOP_NEW_PASSWORD",
+            confirm=True,
+        )
+        response = self.client.opensearch_security_reset(
+            project=self.get_project(),
+            service=self.args.service_name,
+            old_password=old_passwd,
+            new_password=new_passwd,
+        )
+        print(response.get("message"))
+
+    @arg.project
+    @arg.service_name
     def service__connector__available(self) -> None:
         """List available Kafka connectors"""
         project_name = self.get_project()
